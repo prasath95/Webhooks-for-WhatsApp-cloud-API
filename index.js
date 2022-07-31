ace40f5aa6f4c7ab5099340e81c3d5284a6d38a4
prasath95 <6sivaprasath@gmail.com>
2022-07-31 09:48:31 +0530

    added subscribe correctly

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.get("/webhook",(req,res)=>{
 
     if(mode && token){
 
-        if(mode==="subcribe" && token===mytoken){
+        if(mode==="subscribe" && token===mytoken){
             res.status(200).send(challange);
         }else{
             res.status(403);
