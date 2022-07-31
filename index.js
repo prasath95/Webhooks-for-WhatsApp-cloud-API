bc8235cf087a0293182a44588e17dec7b8945c35
prasath95 <6sivaprasath@gmail.com>
2022-07-31 09:57:29 +0530

    message to messages and added many logs

--- index.js
+++ index.js
@@ -38,14 +38,19 @@ app.post("/webhook",(req,res)=>{ //i want some
     console.log(JSON.stringify(body_param,null,2));
 
     if(body_param.object){
+        console.log("inside body param");
         if(body_param.entry && 
             body_param.entry[0].changes && 
-            body_param.entry[0].changes[0].value.message && 
-            body_param.entry[0].changes[0].value.message[0]  
+            body_param.entry[0].changes[0].value.messages && 
+            body_param.entry[0].changes[0].value.messages[0]  
             ){
                let phon_no_id=body_param.entry[0].changes[0].value.metadata.phone_number_id;
                let from = body_param.entry[0].changes[0].value.messages[0].from; 
                let msg_body = body_param.entry[0].changes[0].value.messages[0].text.body;
+
+               console.log("phone number "+phon_no_id);
+               console.log("from "+from);
+               console.log("boady param "+msg_body);
 
                axios({
                    method:"POST",
